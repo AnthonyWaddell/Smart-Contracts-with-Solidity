b14596378c4713192d3f84e690a7c7bb0903b43a
Anthony <23566571+AnthonyWaddell@users.noreply.github.com>
2018-08-08 16:16:05 -0700

    Added modifiers and payable keyword

--- kennel.sol
+++ kennel.sol
@@ -7,14 +7,19 @@ import './DogContract.sol';
 
 contract Kennel is DogContract{
     
+    modifier costs(uint value){
+        require(msg.value >= value); 
+            _;
+    }
+    
     // To transfer ownership of dog object
-    function transferDog(address _newOwner)returns (uint){
+    function transferDog(address _newOwner) payable costs(100) {
         address owner = msg.sender; // get address of old owner
-        require(owner != _newOwner); // Make sure we are not sending to ourselves
+        require(owner != _newOwner); // Stop execution is sending dog to ourself
         uint dogID = ownerToDog[owner]; // get dog id of owner
         delete ownerToDog[owner]; // delete old owner
         ownerToDog[_newOwner] = dogID;
-        assert(ownerToDog[owner] == 0); // Error handling
+        assert(ownerToDog[owner] == 0);
     }
     
     // To allow a Kennel to add dogs, even if they don't "own" them
