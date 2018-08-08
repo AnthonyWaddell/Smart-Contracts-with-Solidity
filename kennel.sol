a52801c4d297154956c98a3aa8d808d1bbf0ba2f
Anthony <23566571+AnthonyWaddell@users.noreply.github.com>
2018-08-08 15:00:34 -0700

    Added error handling statements

--- kennel.sol
+++ kennel.sol
@@ -8,11 +8,13 @@ import './DogContract.sol';
 contract Kennel is DogContract{
     
     // To transfer ownership of dog object
-    function transferDog(address _newOwner){
+    function transferDog(address _newOwner)returns (uint){
         address owner = msg.sender; // get address of old owner
+        require(owner != _newOwner); // Make sure we are not sending to ourselves
         uint dogID = ownerToDog[owner]; // get dog id of owner
         delete ownerToDog[owner]; // delete old owner
         ownerToDog[_newOwner] = dogID;
+        assert(ownerToDog[owner] == 0); // Error handling
     }
     
     // To allow a Kennel to add dogs, even if they don't "own" them
